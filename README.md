3d7044bc9621b5ab7f31194a6a92f2c7213f453b
Zack Harley <zackharley@gmail.com>
2016-08-11 11:43:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# rick-and-morty-cli {
+
+> Get [Rick and Morty][1] gifs
+
+## Install
+
+```
+$ npm install --global rick-and-morty-cli
+```
+
+## Usage
+
+```
+$ rick-and-morty --help
+```
+
+## Related
+
+ - [rick-and-morty][2] - API for this module
+
+## License
+
+2016 © [Zack Harley][3]
+> :fork_and_knife: Fork away!
+
+# }
+
+[1]: https://en.wikipedia.org/wiki/Rick_and_Morty
+[2]: https://github.com/zackharley/rick-and-morty
+[3]: https://github.com/zackharley
---
+++ cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const rickAndMorty = require('rick-and-morty');
+const open = require('opn');
+
+const cli = meow(`
+	Examples
+	  $ rick-and-morty
+	  http://i.giphy.com/l41lI4bYmcsPJX9Go.gif
+	  $ rick-and-morty --all
+	  http://i.giphy.com/l41lI4bYmcsPJX9Go.gif
+	  http://i.giphy.com/2s0ouek7HJmWQ.gif
+	  ...
+	  $ rick-and-morty -p
+	  // -> Opens in your browser
+
+	Options
+	  --all  Get all of the GIF URLs
+	  --play, -p  Play a random GIF
+`, {
+	alias: {
+		p: 'play'
+	}
+});
+
+if (cli.flags.all && cli.flags.play) {
+	console.log('Sorry, I can\'t play all of the GIFs');
+} else if (cli.flags.all) {
+	console.log(rickAndMorty.all.join('\n'));
+} else if (cli.flags.play) {
+	open(rickAndMorty.random());
+} else {
+	console.log(rickAndMorty.random());
+}
